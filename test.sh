18aea5539f408a17db3e740a46062a5e60433727
wenhe-jia <jiawenhe3991@163.com>
2022-11-28 23:55:30 +0800

    Delete test.sh

--- test.sh
+++
@@ -1,5 +0,0 @@
-python train_net.py \
-  --config-file configs/coco_wo_lsj/maskformer2_R50_bs16_12ep_projpair.yaml \
-  --eval-only \
-  --num-gpus 4 \
-  MODEL.WEIGHTS coco-ins-projection-weightup-12ep/model_final.pth
