cee1eded50430658f98878b136caaf6b9a038a33
wenhe-jia <jiawenhe3991@163.com>
2022-11-28 23:55:22 +0800

    Delete debug.sh

--- debug.sh
+++
@@ -1,8 +0,0 @@
-CUDA_VISIBLE_DEVICES=0 python train_net.py \
-  --dist-url tcp://127.0.0.1:$(( RANDOM % 1000 + 20100 )) \
-  --config-file configs/coco_wo_lsj/maskformer2_R50_bs16_12ep_projpair.yaml \
-  --num-gpus 1 \
-  SOLVER.MAX_ITER 10 \
-  SOLVER.IMS_PER_BATCH 2 \
-  DATALOADER.NUM_WORKERS 0 \
-  OUTPUT_DIR debug-ProjPair
